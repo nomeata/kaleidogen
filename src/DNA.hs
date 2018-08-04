0cfb3607a8bd159c6121fbdec061937e31b4c88d
Joachim Breitner <mail@joachim-breitner.de>
2018-08-04 17:48:32 -0400

    More random combination

--- src/DNA.hs
+++ src/DNA.hs
@@ -1,9 +1,23 @@
 module DNA where
 
+import Control.Monad.Random.Strict
+import Data.Hashable
+
+
 type DNA = [Int]
 
 crossover :: DNA -> DNA -> DNA
-crossover x y = x ++ y ++ [sum x + sum y]
+crossover x y
+    | null x = map (+1) y
+    | null y = map (+2) x
+    | length x + length y <= 2 = x ++ y ++ [sum x + sum y]
+    | otherwise
+    = flip evalRand initG $ (join . weighted)
+        [ (pure $ x ++ y ++ [sum x + sum y], 1)
+        , (pure $ zipWith (+) x y, 1)
+        ]
+  where
+    initG = mkStdGen (hash (x,y))
 
 initialDNAs :: [DNA]
 initialDNAs =
