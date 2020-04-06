d4c2768b24598bb3fa57b3abb79da526b192c947
Joachim Breitner <mail@joachim-breitner.de>
2020-04-06 19:48:45 +0200

    Clean up Img

--- src/Img.hs
+++ src/Img.hs
@@ -5,12 +5,8 @@
 module Img where
 
 import Data.Complex
-import Data.Colour
-import Data.Colour.CIE
 import Data.Colour.SRGB
-import Data.Colour.Names
 import qualified Data.ByteString.Lazy as BS
-import GHC.Exts
 
 import Codec.Picture
 
@@ -46,13 +42,13 @@ tween x (C a1 b1 c1) (C a2 b2 c2) = C
   ((1 - x) * c1 + x * c2)
 
 go :: RNA -> Pattern
-go _ (!pos) | False = undefined
+go _ (!_pos) | False = undefined
 
 go (Solid color) _pos = C r g b
   where
     RGB r g b = color
 
-go (Blend x r1 r2) _pos = error "blend"
+go Blend{} _pos = error "blend"
 
 go (Checker x r1 r2) pos = do
     let !(tmpx :+ tmpy) = realToFrac x * ((1.0 :+ 0.0) + pos)
@@ -121,47 +117,17 @@ go (Ontop x r1 r2) pos = do
 
 
 img2Png :: Img -> BS.ByteString
-img2Png i = encodePng $ generateImage go w h
+img2Png i = encodePng $ generateImage colorAt w h
   where
     w = 1000
     h = 1000
-    go :: Int -> Int -> PixelRGBA8
-    go x y = do
+    colorAt :: Int -> Int -> PixelRGBA8
+    colorAt x y = do
       let x' = 2 * fromIntegral x / fromIntegral w - 1
       let y' = 2 * fromIntegral y / fromIntegral h - 1
       case i (y' :+ x') of
         Nothing -> PixelRGBA8 0 0 0 0
-        Just (C x y z) -> PixelRGBA8 (clamp x) (clamp y) (clamp z) 255
+        Just (C r g b) -> PixelRGBA8 (clamp r) (clamp g) (clamp b) 255
 
     clamp :: Double -> Pixel8
     clamp = fromIntegral . max (0::Int) . min 255 . round . (* 256)
-
--- For reference: Parts of the rendering code
-
-{-
-render :: Int -> Int -> Img -> BS.ByteString
-render w h f = BS.pack $ concat
-    [ [fromIntegral r,fromIntegral g,fromIntegral b,0xff]
-    | y <- [0..h-1]
-    , x <- [0..w-1]
-    , let x' = s * fromIntegral (x - (w`div`2))
-    , let y' = s * fromIntegral (y - (h`div`2))
-    , let RGB r g b = toSRGB24 $ f (x' :+ y')
-    ]
-  where s = 2 / min (fromIntegral w) (fromIntegral h)
-
-paint :: JSVal -> Img -> JSM ()
-paint canvas f = do
-    Just w <- canvas ^. js "width" >>= fromJSVal
-    Just h <- canvas ^. js "height" >>= fromJSVal
-    --jsg "console" ^. js1 "log" (w,h)
-    ctx <- canvas ^. js1 "getContext" "2d"
-    (buf, _, _) <- ghcjsPure $ B.fromByteString $ render w h f
-    array <- ghcjsPure (B.getUint8Array buf)
-    array <- ghcjsPure (jsval array)
-    array <- new (jsg "Uint8ClampedArray") array
-    imgData <- new (jsg "ImageData") (array,w,h)
-    ctx ^. js3 "putImageData" imgData (0::Int) (0::Int)
-    return ()
--}
-
