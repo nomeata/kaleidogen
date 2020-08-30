cbc1fff34b3d0edb7b57d8531439839135e351f1
Joachim Breitner <mail@joachim-breitner.de>
2020-08-30 10:37:57 +0200

    niv-ify

--- default.nix
+++ default.nix
@@ -1,5 +1,10 @@
 let
-  pkgs = import ~/build/nixpkgs {
+  sources = import (builtins.fetchurl {
+    url = https://raw.githubusercontent.com/nmattia/niv/v0.2.16/nix/sources.nix;
+    sha256 = "03fl8wfm2nhdiws7pmfz2kcbf47mv2f8gk30fzg4m07gb5zdv6gv";
+  }) { sourcesFile = ./nix/sources.json; };
+
+  pkgs = import sources.nixpkgs {
     config = {
       packageOverrides = pkgs: rec {
         haskellPackages = pkgs.haskellPackages.override {
@@ -27,7 +32,7 @@ let
     sdl2 = pkgs.haskell.lib.dontCheck super.sdl2;
   };
 
-  platform = import /home/jojo/build/haskell/reflex/reflex-platform {
+  platform = import sources.reflex-platform {
     config = {
       android_sdk = {
         accept_license = true;
