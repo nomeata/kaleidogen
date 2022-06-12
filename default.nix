693579b8fd9ae52711be8c26284f474c9c47e089
Joachim Breitner <mail@joachim-breitner.de>
2022-06-12 11:22:17 +0200

    Remove unused line

--- default.nix
+++ default.nix
@@ -86,8 +86,6 @@ let
 
   staticHaskellPackages = with pkgs.haskell.lib; staticPkgs.haskellPackages.override {
     overrides = self: super: {
-      # Dependencies we need to patch
-      hpc-coveralls = appendPatch super.hpc-coveralls (builtins.fetchurl https://github.com/guillaume-nargeot/hpc-coveralls/pull/73/commits/344217f513b7adfb9037f73026f5d928be98d07f.patch);
       telegram-api = self.callPackage telegram-api-pkg {};
       aws-lambda-haskell-runtime = pkgs.haskell.lib.appendPatch
         (unmarkBroken super.aws-lambda-haskell-runtime)
